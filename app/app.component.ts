4b6c80b9d1f99a8e50533f58995b8181de5ce99a
Alexander Ziskind <alex@nuvious.com>
2017-03-18 12:40:48 -0400

    Components

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "ns-app",
-    template: '<Label text="To Be Replaced"></Label>'
+    template: '<pt-backlog></pt-backlog>'
 
 })
 export class AppComponent { }
--- app/pt-backlog/pt-backlog.module.ts
+++ app/pt-backlog/pt-backlog.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 
+//nativescript imports
+import { NativeScriptModule } from "nativescript-angular/platform";
+
 import { PTBacklogComponent } from './pt-backlog.component';
+import { PTItemListComponent } from './pt-item-list/pt-item-list.component';
 import { PipesModule } from './pipes/pipes.module';
 
 @NgModule({
-    imports: [PipesModule],
-    exports: [],
-    declarations: [PTBacklogComponent],
+    imports: [
+        NativeScriptModule,
+        PipesModule
+    ],
+    exports: [PTBacklogComponent],
+    declarations: [
+        PTBacklogComponent,
+        PTItemListComponent
+    ],
     providers: [],
 })
 export class PTBacklogModule { }
