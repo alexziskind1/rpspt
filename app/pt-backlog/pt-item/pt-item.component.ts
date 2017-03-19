354341fb94169cdaa891838163f632abd1346163
Alexander Ziskind <alex@nuvious.com>
2017-03-19 13:43:05 -0400

    Added title display to item page

--- app/pt-backlog/pt-item/pt-item.component.ts
+++ app/pt-backlog/pt-item/pt-item.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ItemTypeEnum, PriorityEnum, StatusEnum } from '../../shared/static-data';
+import { PTDomain } from '../../typings/domain';
+import IPTItem = PTDomain.IPTItem;
+
+
 @Component({
     moduleId: module.id,
     selector: 'pt-item',
-    templateUrl: 'pt-item.component.html'
+    templateUrl: 'pt-item.component.html',
+    styleUrls: ['pt-item.component.css']
 })
 export class PTItemComponent implements OnInit {
+
+    public item: IPTItem;
+    public get itemTitle() {
+        return this.item ? this.item.title : '';
+    }
+
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.item = ITEM;
+    }
 }
+
+const ITEM: IPTItem =
+    { id: '1', title: 'item 1', description: 'item 1 desc', estimate: 5, priority: PriorityEnum.Low, status: StatusEnum.Open, tasks: [], type: ItemTypeEnum.Bug, dateCreated: new Date(), dateModified: new Date(), comments: [], assignee: null };
