be9073746f0cfaac4a701c0fdf884042c381ad6e
Alexander Ziskind <alex@nuvious.com>
2017-03-21 08:51:58 -0400

    Status bar utils - iOS implementation

--- app/shared/status-bar-util.ts
+++ app/shared/status-bar-util.ts
@@ -1,7 +1,16 @@
 import * as application from "application";
+import * as platform from 'platform';
+import * as utils from "utils/utils";
+
+declare var UIStatusBarStyle: any;
+declare var UIApplication: any;
 
 export function setStatusBarColors() {
-    application.on(application.launchEvent, () => {
-        console.log('APP LAUNCHED');
-    });
+    if (platform.isIOS) {
+        application.on(application.launchEvent, () => {
+            utils.ios.getter(UIApplication, UIApplication.sharedApplication).statusBarStyle = UIStatusBarStyle.LightContent;
+        });
+    } else if (platform.isAndroid) {
+
+    }
 }
