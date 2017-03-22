5aad107edbfa1e3c393ba6375cfc9c5dac03d1cc
Alexander Ziskind <alex@nuvious.com>
2017-03-22 09:40:16 -0400

    Removed dynamic binding elements -- not till next segment

--- app/pt-backlog/pt-item/pt-item.component.ts
+++ app/pt-backlog/pt-item/pt-item.component.ts
@@ -19,25 +19,13 @@ import { ItemTypePickerModalComponent } from "../shared/item-type-picker-modal.c
 })
 export class PTItemComponent implements OnInit {
 
-    private _itemDetailScreens = [
-        { title: 'Details', routePath: 'pt-item-details' },
-        { title: 'Tasks', routePath: 'pt-item-tasks' },
-        { title: 'Chitchat', routePath: 'pt-item-chitchat' }
-    ];
-    public myNavItems: Array<SegmentedBarItem> = [];
     public item: IPTItem;
 
     constructor(
         private backlogService: BacklogService,
         private modalService: ModalDialogService,
         private vcRef: ViewContainerRef
-    ) {
-        for (let i = 0; i < this._itemDetailScreens.length; i++) {
-            let tmpSegmentedBarItem: SegmentedBarItem = <SegmentedBarItem>new SegmentedBarItem();
-            tmpSegmentedBarItem.title = this._itemDetailScreens[i].title;
-            this.myNavItems.push(tmpSegmentedBarItem);
-        }
-    }
+    ) { }
 
     ngOnInit() {
         this.backlogService.getItem('2')
