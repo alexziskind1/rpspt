c8c930d9949695cc4f355629b83bd8e82c7055f3
Alexander Ziskind <alex@nuvious.com>
2017-03-26 12:36:15 -0400

    Added imperative animation forward

--- app/pt-login/pt-login.component.ts
+++ app/pt-login/pt-login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 import { Page } from 'ui/page';
 import { View } from 'ui/core/view';
+import { Color } from "color";
+import * as enums from 'ui/enums';
 
 import { AuthenticationService } from '../services';
 import { DEMO_PASSWORD } from '../shared/constants';
@@ -17,9 +19,17 @@ import ILoginModel = PTDomain.ILoginModel;
 export class LoginComponent implements OnInit {
     public isLoading: boolean = false;
     @ViewChild('loginInputs') loginInputsRef: ElementRef;
+    @ViewChild('btnLoginWrapper') btnLoginWrapperRef: ElementRef;
+    @ViewChild('btnLogin') btnLoginRef: ElementRef;
 
     public get loginInputs(): View {
         return this.loginInputsRef.nativeElement;
+    }
+    public get btnLoginWrapper(): View {
+        return this.btnLoginWrapperRef.nativeElement;
+    }
+    public get btnLogin(): View {
+        return this.btnLoginRef.nativeElement;
     }
 
     public loginModel: ILoginModel = { username: 'alexziskind', password: 'bad_password' };
@@ -37,6 +47,8 @@ export class LoginComponent implements OnInit {
         this.isLoading = true;
         this.loginInputs.className = '';
 
+        this.loginAnimationForward();
+
         this.authService.login(this.loginModel.username, this.loginModel.password)
             .subscribe(data => {
                 if (data === null) {
@@ -51,5 +63,26 @@ export class LoginComponent implements OnInit {
             error => {
                 this.isLoading = false;
             });
+    }
+
+    private loginAnimationForward() {
+        this.btnLogin.opacity = 0;
+        this.btnLoginWrapper.animate({
+            scale: { x: 0.5, y: 0.5 },
+            duration: 200,
+            curve: enums.AnimationCurve.cubicBezier(0, .75, .22, 1)
+        }).then(() => {
+            this.btnLoginWrapper.animate({
+                scale: { x: 20, y: 20 },
+                duration: 250,
+                curve: enums.AnimationCurve.cubicBezier(.93, .02, 1, .25)
+            }).then(() => {
+                this.btnLoginWrapper.animate({
+                    backgroundColor: new Color('#999999'),
+                    duration: 5000,
+                    delay: 500
+                });
+            });
+        });
     }
 }
