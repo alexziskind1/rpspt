390b010075e0116cad33ff333982d719cf446fb7
Alexander Ziskind <alex@nuvious.com>
2017-03-28 18:53:24 -0400

    Using mocked items instead of hard coded items

--- app/services/backlog.service.ts
+++ app/services/backlog.service.ts
@@ -27,8 +27,8 @@ export class BacklogService {
         private mockDataService: MockDataService,
         private userService: UserService
     ) {
-        this._allItems = ITEMS;
-        this._allItems[0].assignee = this.userService.users[0];
+        this._allItems = mockDataService.generatePTItems(userService.users);
+        //this._allItems[0].assignee = this.userService.users[0];
     }
 
     public getItem(id: string) {
@@ -55,11 +55,3 @@ export class BacklogService {
     }
 }
 
-
-const ITEMS: IPTItem[] = [
-    { id: '1', title: 'item 1', description: 'item 1 desc', estimate: 5, priority: PriorityEnum.Low, status: StatusEnum.Open, tasks: [], type: ItemTypeEnum.Bug, dateCreated: new Date(), dateModified: new Date(), comments: [], assignee: null },
-    { id: '2', title: 'item 2', description: 'item 2 desc', estimate: 10, priority: PriorityEnum.Medium, status: StatusEnum.ReOpened, tasks: [], type: ItemTypeEnum.PBI, dateCreated: new Date(), dateModified: new Date(), comments: [], assignee: null },
-    { id: '3', title: 'item 3', description: 'item 3 desc', estimate: 12, priority: PriorityEnum.High, status: StatusEnum.Open, tasks: [], type: ItemTypeEnum.Chore, dateCreated: new Date(), dateModified: new Date(), comments: [], assignee: null },
-    { id: '4', title: 'item 4', description: 'item 4 desc', estimate: 6, priority: PriorityEnum.Critical, status: StatusEnum.Open, tasks: [], type: ItemTypeEnum.Impediment, dateCreated: new Date(), dateModified: new Date(), comments: [], assignee: null },
-    { id: '5', title: 'item 5', description: 'item 5 desc', estimate: 20, priority: PriorityEnum.Low, status: StatusEnum.Open, tasks: [], type: ItemTypeEnum.Bug, dateCreated: new Date(), dateModified: new Date(), comments: [], assignee: null }
-];
